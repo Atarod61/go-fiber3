12ba2820f40937c1c61dbdedcdef5c75cda08907
Atarod61 <atarod2017@gmail.com>
2024-12-16 18:23:16 +0330

    Improve feature

--- seeders/userseeder.go
+++ seeders/userseeder.go
@@ -1,5 +1,19 @@
 package seeders
 
+import (
+	"log"
+
+	db "github.com/Atarod61/go-fiber3/database"
+)
+
 func seedusers() {
+
+	dbClient := db.GetDB()
+
+	error := dbClient.create().Error
+
+	if err != nil {
+		log.Fatalf("user cannot be added to database")
+	}
 
 }
