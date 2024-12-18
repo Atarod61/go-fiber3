88ac2cb6b154176f78ad05cfcf80c8eb3e2feca2
Atarod61 <atarod2017@gmail.com>
2024-12-18 18:44:10 +0330

    Update file

--- models/user.go
+++ models/user.go
@@ -1,7 +1,8 @@
 package models
 
 type user struct {
-	username string
-	password string
-	Email    string
+	Id unit "gorm: primarykey;auto-increment"
+	Username string "jason: "username" gorm:"unique"'
+	Password string "jason: "password" gorm:"type:varchar(10)"
+	Email    string "jason: "email"gorm:"unique""
 }
