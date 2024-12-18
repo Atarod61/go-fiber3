5bd44fcbf1057c3755e76509a73d1f160cb9436c
Atarod61 <atarod2017@gmail.com>
2024-12-18 18:50:41 +0330

    Optimize feature

--- models/user.go
+++ models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type user struct {
-	Id unit "gorm: primarykey;auto-increment"
+	Id unit "jason: "id" gorm: primarykey;auto-increment"
 	Username string "jason: "username" gorm:"unique"'
 	Password string "jason: "password" gorm:"type:varchar(10)"
 	Email    string "jason: "email"gorm:"unique""
