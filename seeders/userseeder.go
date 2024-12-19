2e5f367e622806e1df89566a88266c55e04e48f0
Atarod61 <atarod2017@gmail.com>
2024-12-19 21:14:38 +0330

    Refactor code

--- seeders/userseeder.go
+++ seeders/userseeder.go
@@ -4,18 +4,24 @@ import (
 	"log"
 
 	db "github.com/Atarod61/go-fiber3/database"
+	"github.com/Atarod61/go-fiber3/models"
 )
 
 func seedusers() {
 
 	dbClient := db.GetDB()
 	// ravesh 1
-	var user models.use
-	user.Email = "test@test.com"
-	user.Username = "test"
-	user.Password = "123456789"
+	//var user models.use
+	//user.Email = "test@test.com"
+	//user.Username = "test"
+	//user.Password = "123456789"
 
 	//ravesh 2
+	User := models.user{
+		Username : "Atarod61",
+		Password : "123456789",
+		Email : "atarod2017@gmail.com"
+	}
 
 	err := dbClient.Create(&user).Error
 
